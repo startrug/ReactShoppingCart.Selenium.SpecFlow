69de04476f218c364bfaa8483c6ed344f1dbe8f9
Artur Strug <artstrug@gmail.com>
2020-05-07 15:00:51 +0200

    F/001: Add sample test

---
+++ Settings/TestsHook.cs
@@ -0,0 +1,35 @@
+﻿using BoDi;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace ReactShoppingCart.Selenium.SpecFlow.Settings
+{
+    [Binding]
+    public class TestsHook
+    {
+        public TestsHook(IObjectContainer objectContainer)
+        {
+            this.objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void InitializeWebDriver()
+        {
+            driver = new ChromeDriver();
+            objectContainer.RegisterInstanceAs(driver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Close();
+            driver.Dispose();
+            objectContainer.Dispose();
+        }
+
+        private IWebDriver driver;
+
+        private readonly IObjectContainer objectContainer;
+    }
+}
